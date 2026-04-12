ff1555d2b286d2da00896727e419e11f9a92660c
Jjethro93 <sufra93@gmail.com>
2026-04-12 18:25:32 -0300

    Responsive correction

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+let btnMenu = document.getElementById('mobile-btn');
+let menu = document.getElementById('mobile-menu')
+
+
+
+
+btnMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+    console.log('click')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
